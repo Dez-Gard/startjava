7e92d460b79c2041dbc34fcf60f1e59f940a6afc
Dez-Gard <dezgard.post@gmail.com>
2022-02-23 00:26:10 +0200

    create "guess number" game

---
+++ StartJava/src/Lesson_1/MyFirstGame.java
@@ -0,0 +1,21 @@
+package Lesson_1;
+
+public class MyFirstGame {
+
+	public static void main(String[] args) {
+
+		int diapason = 100;
+		int randomNumber = (int) (Math.random() * diapason);
+
+		for (int niceTry = 100; niceTry <= 100; niceTry--) {
+			if (niceTry > randomNumber) {
+				System.out.println(niceTry + " Данное число больше того, что загадал компьютер");
+			} else if (niceTry < randomNumber) {
+				System.out.println(niceTry + " Данное число меньше того, что загадал компьютер");
+			} else if (niceTry == randomNumber) {
+				System.out.println("Поздравляю, число угадано! - " + niceTry);
+				break;
+			}
+		}
+	}
+}
