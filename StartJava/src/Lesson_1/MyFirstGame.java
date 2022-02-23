f4b4d5917b923141e4326dbc4de2c6289e1e5864
Dez-Gard <dezgard.post@gmail.com>
2022-02-23 15:11:11 +0200

    refactoring after video

--- StartJava/src/Lesson_1/MyFirstGame.java
+++ StartJava/src/Lesson_1/MyFirstGame.java
@@ -4,17 +4,21 @@ public class MyFirstGame {
 
 	public static void main(String[] args) {
 
-		int randomNumber = 67;
+		int computerNumber = 67;
+		int myNumber = 50;
 
-		for (int niceTry = 100; niceTry <= 100; niceTry--) {
-			if (niceTry > randomNumber) {
-				System.out.println(niceTry + " Данное число больше того, что загадал компьютер");
-			} else if (niceTry < randomNumber) {
-				System.out.println(niceTry + " Данное число меньше того, что загадал компьютер");
-			} else if (niceTry == randomNumber) {
-				System.out.println("Поздравляю, число угадано! - " + niceTry);
-				break;
+		System.out.println("Стартуем!");
+
+		while (myNumber != computerNumber) {
+			if (myNumber > computerNumber) {
+				System.out.println(myNumber + " Данное число больше того, что загадал компьютер");
+				myNumber--;
+			}
+			if (myNumber < computerNumber) {
+				System.out.println(myNumber + " Данное число меньше того, что загадал компьютер");
+				myNumber++;
 			}
 		}
+		System.out.println("Вы угадали! Это число " + computerNumber);
 	}
 }
